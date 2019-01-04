1c06533f86a6f7403e712914e06f10b0848c6c4e
tridon777 <tridon777@gmail.com>
2019-01-03 20:21:20 -0500

    URL Error Handling

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-let loading = async (input) => await fetch('/', {
+let throwError = error => { throw new Error(error)}
+
+let loading = async input => await fetch('/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json'},
     body: JSON.stringify({
         urlInput: input
     })
-}).then(data => data.blob()).then(data => {
-    let downloadTag = document.createElement('a')
-    downloadTag.target = '_blank'
-    downloadTag.download = 'save.pdf'
-    let newBlob = new Blob([data], {
-        type: "application/pdf"
-    })
-    let thisData = window.URL.createObjectURL(newBlob)
-    downloadTag.href = thisData
-    downloadTag.click()
 })
+    .then(res => res.ok ? res : throwError(`${res.status},${res.statusText}` ))
+    .then(data => data.blob())
+    .then(data => {
+        let downloadTag = document.createElement('a')
+        downloadTag.target = '_blank'
+        downloadTag.download = 'save.pdf'
+        let newBlob = new Blob([data], {
+            type: "application/pdf"
+        })
+        let thisData = window.URL.createObjectURL(newBlob)
+        downloadTag.href = thisData
+        downloadTag.click()
+    })
+    .catch(error => {
+        console.log(error)
+    })
 
 document.getElementById('urlForm').addEventListener('submit',(e)=>{
-    var test = e.target.querySelectorAll('input#urlInput')[0].value
-        debugger
+    var urlInput = e.target.querySelectorAll('input#urlInput')[0].value
     e.preventDefault()
-    loading(test)
+    loading(urlInput)
 })
--- server.ts
+++ server.ts
@@ -4,23 +4,33 @@ import RenderPDF from 'chrome-headless-render-pdf'
 
 const port: number = Number(process.env.PORT) || 3000
 const app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('.'))
-app.get('/',(req: express.Request,res: express.Response) => {
+app.get('/',(req: express.Request,res: express.Response) =>
     res.sendFile('index.html',{root: __dirname})
-})
+)
+
+const checkProtocolExists = (url: string) => /^(https?:\/\/)?/i.test(url)
+
+const validURL = (url: string) => /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i.test(url)
 
 app.post('/', (req: express.Request, res: express.Response) => {
     let urlInput = req.body.urlInput
-    const httpReg = RegExp('^http://')
-    const httpsReg = RegExp('^https://')
-    let url = httpReg.test(urlInput) || httpsReg.test(urlInput) ? urlInput : `http://${urlInput}`
-    RenderPDF.generatePdfBuffer(url)
-        .then((pdfBuffer) => {
-            res.set('Content-Type','application/pdf')
-            res.send(pdfBuffer)
-        });
+    let urlProtocolCheck = checkProtocolExists(urlInput) ? urlInput : `http://${urlInput}`
+    validURL(urlProtocolCheck) ?
+        RenderPDF
+            .generatePdfBuffer(urlProtocolCheck)
+            .then((pdfBuffer: Buffer) =>
+                res.set('Content-Type','application/pdf').send(pdfBuffer)
+            )
+            .catch((error: string) =>
+                res.status(500).send(error)
+            )
+    :
+        res.statusMessage = 'Invalid URL'
+        res.status(500).send()
 })
 
 app.listen(port, () => {
