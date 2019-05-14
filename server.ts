870c7e1fd056578eb93255ef913383f7ca2804f4
Paul Searcy <psearcy@dhs.state.ia.us>
2019-05-14 15:18:55 -0500

    - updating tsconfig - updating protocol regex to be more strict - updating endpoint to be async and await for chrome headless pdf - resolving how express js handles conditionals and error

--- server.ts
+++ server.ts
@@ -14,24 +14,21 @@ app.get('/',(req: express.Request,res: express.Response) =>
     res.sendFile('index.html',{root: __dirname})
 )
 
-const checkProtocolExists = (url: string) => /^(https?:\/\/)?/i.test(url)
+const checkProtocolExists = (url: string) => /^(https?:\/\/)/i.test(url)
 
 const validURL = (url: string) => /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i.test(url)
 
-app.post('/', (req: express.Request, res: express.Response) => {
+app.post('/', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     let urlInput = req.body.urlInput
     let urlProtocolCheck = checkProtocolExists(urlInput) ? urlInput : `http://${urlInput}`
     validURL(urlProtocolCheck) ?
-        RenderPDF
-            .generatePdfBuffer(urlProtocolCheck)
+       await RenderPDF
+            .generatePdfBuffer(urlProtocolCheck,{"chromeBinary" : "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" })
             .then((pdfBuffer: Buffer) =>
                 res.set('Content-Type','application/pdf').send(pdfBuffer)
             )
-            .catch((error: string) =>
-                res.status(500).send(error)
-            )
+            .catch(next)
     :
-        res.statusMessage = 'Invalid URL'
         res.status(500).send()
 })
 
