88dde2374ee85ae5c5344ade7e39764f40af32a9
PaulSearcy <tridon777@gmail.com>
2019-05-21 16:37:09 -0500

    - ignoring .vscode for now - updating server-side to check OS for chrome location

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import path from 'path'
 import RenderPDF from 'chrome-headless-render-pdf'
+import os from 'os'
 
 const port: number = Number(process.env.PORT) || 3000
 const app = express()
@@ -18,18 +19,29 @@ const checkProtocolExists = (url: string) => /^(https?:\/\/)/i.test(url)
 
 const validURL = (url: string) => /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i.test(url)
 
+const createBuffer = async (url:string) =>
+    os.platform() === 'win32' ?
+        await RenderPDF
+            .generatePdfBuffer(url,{"chromeBinary" : "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" })
+    :
+        await RenderPDF
+            .generatePdfBuffer(url)
+
+
 app.post('/', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     let urlInput = req.body.urlInput
     let urlProtocolCheck = checkProtocolExists(urlInput) ? urlInput : `http://${urlInput}`
-    validURL(urlProtocolCheck) ?
-       await RenderPDF
-            .generatePdfBuffer(urlProtocolCheck,{"chromeBinary" : "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" })
-            .then((pdfBuffer: Buffer) =>
+    if(validURL(urlProtocolCheck)) {
+        try {
+            createBuffer(urlProtocolCheck).then((pdfBuffer: any) =>
                 res.set('Content-Type','application/pdf').send(pdfBuffer)
             )
-            .catch(next)
-    :
+        } catch(error){
+            next
+        }
+    }else{
         res.status(500).send()
+    }
 })
 
 app.listen(port, () => {
