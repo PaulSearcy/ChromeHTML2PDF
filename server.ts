7de3fbdd10dd208629505a01d3e368b7c6f7958d
PaulSearcy <tridon777@gmail.com>
2019-05-21 16:41:18 -0500

    - for now reverting back to linux only chrome auto detect

--- server.ts
+++ server.ts
@@ -19,26 +19,32 @@ const checkProtocolExists = (url: string) => /^(https?:\/\/)/i.test(url)
 
 const validURL = (url: string) => /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i.test(url)
 
-const createBuffer = async (url:string) =>
-    os.platform() === 'win32' ?
-        await RenderPDF
-            .generatePdfBuffer(url,{"chromeBinary" : "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" })
-    :
-        await RenderPDF
-            .generatePdfBuffer(url)
+// const createBuffer = async (url:string) =>
+//     os.platform() === 'win32' ?
+//         await RenderPDF
+//             .generatePdfBuffer(url,{"chromeBinary" : "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe" })
+//     :
+//         await RenderPDF
+//             .generatePdfBuffer(url)
 
 
 app.post('/', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     let urlInput = req.body.urlInput
     let urlProtocolCheck = checkProtocolExists(urlInput) ? urlInput : `http://${urlInput}`
     if(validURL(urlProtocolCheck)) {
-        try {
-            createBuffer(urlProtocolCheck).then((pdfBuffer: any) =>
+        // try {
+        //     createBuffer(urlProtocolCheck).then((pdfBuffer: any) =>
+        //         res.set('Content-Type','application/pdf').send(pdfBuffer)
+        //     )
+        // } catch(error){
+        //     next
+        // }
+       await RenderPDF
+            .generatePdfBuffer(urlProtocolCheck)
+            .then((pdfBuffer: Buffer) =>
                 res.set('Content-Type','application/pdf').send(pdfBuffer)
             )
-        } catch(error){
-            next
-        }
+            .catch(next)
     }else{
         res.status(500).send()
     }
